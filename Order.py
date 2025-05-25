a972bb91c2d7cfcb732fc56241d4f830d8cbc7bf
ratam1703 <rasyadhimas@gmail.com>
2025-05-25 17:51:01 +0700

    sesi 11

---
+++ Order.py
@@ -0,0 +1,8 @@
+import uuid
+from datetime import datetime
+import random
+
+def generate_order_id():
+    tanggal = datetime.now().strftime("%Y%m%d")
+    random_number = random.randint(100000, 999999)
+    return f"ORDER-{tanggal}-{random_number}"
